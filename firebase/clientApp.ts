8753f12d8a82fd09715631b836303930ac76b0a2
Halil Ibrahim Gercek <208321685+Higer23@users.noreply.github.com>
2026-07-27 14:56:51 +0200

    Add Firebase Realtime Database to clientApp

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -2,25 +2,23 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBCymaJvnsJpAVta8JrHAGIN-VOq5oRGug",
+  authDomain: "hig-um.firebaseapp.com",
+  databaseURL: "https://hig-um-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hig-um",
+  storageBucket: "hig-um.firebasestorage.app",
+  messagingSenderId: "806579726040",
+  appId: "1:806579726040:web:2bd2ae173d3bfd3f9999ad",
 };
 
-/**
- * Initializes the Firebase client SDK with the provided configuration.
- * Ensures that the Firebase app is only initialized once, even during Next.js hot reloads or hydration.
- * Exports the initialized Firestore, Auth, and Storage services for use throughout the application.
- */
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 const firestore = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+const database = getDatabase(app);
 
-export { firestore, auth, storage };
+export { firestore, auth, storage, database };

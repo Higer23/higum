97d9c992f4af7c59912a9e085a86efb90bab9706
Halil Ibrahim Gercek <208321685+Higer23@users.noreply.github.com>
2026-07-27 03:41:03 +0200

    Refactor usePostsFeed for improved state management

--- hooks/posts/usePostsFeed.ts
+++ hooks/posts/usePostsFeed.ts
@@ -12,24 +12,18 @@ type UsePostsFeedProps = {
   isGenericHome?: boolean;
 };
 
-/**
- * A custom hook that manages the post feed for communities and the home page.
- * It handles paginated fetching of posts.
- * @param communityId - Optional identifier to fetch posts for a specific community.
- * @param communityIds - Optional array of identifiers to fetch posts for a personalized home feed.
- * @param isGenericHome - Optional flag to fetch posts for the generic home feed.
- * @returns An object containing the loading state, fetchPosts function, and a flag for no more posts.
- */
 const usePostsFeed = ({
   communityId,
   communityIds,
   isGenericHome,
 }: UsePostsFeedProps) => {
   const setPostStateValue = useSetAtom(postStateAtom);
+
   const [loading, setLoading] = useState(false);
   const [lastVisible, setLastVisible] =
     useState<QueryDocumentSnapshot<DocumentData> | null>(null);
   const [noMorePosts, setNoMorePosts] = useState(false);
+
   const showToast = useCustomToast();
 
   const fetchPosts = async (initial = false) => {
@@ -38,6 +32,7 @@ const usePostsFeed = ({
     if (!initial && !lastVisible) return;
 
     setLoading(true);
+
     try {
       const { posts, newLastVisible } = await getPostsLib(
         communityId,
@@ -46,23 +41,30 @@ const usePostsFeed = ({
         initial ? null : lastVisible
       );
 
-      if (posts.length < 10) setNoMorePosts(true);
-      if (newLastVisible) setLastVisible(newLastVisible);
-      else if (initial) {
-        setNoMorePosts(true);
+      if (initial) {
+        setPostStateValue((prev) => ({
+          ...prev,
+          posts: posts as Post[],
+        }));
+      } else {
+        setPostStateValue((prev) => ({
+          ...prev,
+          posts: [...prev.posts, ...(posts as Post[])],
+        }));
       }
 
-      setPostStateValue((prev) => ({
-        ...prev,
-        posts: initial
-          ? (posts as Post[])
-          : [...prev.posts, ...(posts as Post[])],
-      }));
+      if (newLastVisible) {
+        setLastVisible(newLastVisible);
+      }
+
+      setNoMorePosts(posts.length < 10);
     } catch (error: any) {
-      console.log("Error: fetchPosts", error);
+      console.error("Firestore Error:", error);
+      alert(error?.message || "Unknown Firestore Error");
+
       showToast({
         title: "Could not Fetch Posts",
-        description: "There was an error fetching posts",
+        description: error?.message || "There was an error fetching posts",
         status: "error",
       });
     } finally {
@@ -73,18 +75,12 @@ const usePostsFeed = ({
   useEffect(() => {
     setNoMorePosts(false);
     setLastVisible(null);
+
     setPostStateValue((prev) => ({
       ...prev,
       posts: [],
     }));
-
-    return () => {
-      setPostStateValue((prev) => ({
-        ...prev,
-        posts: [],
-      }));
-    };
-  }, [communityId, isGenericHome, setPostStateValue]);
+  }, [communityId, communityIds, isGenericHome, setPostStateValue]);
 
   return {
     loading,
